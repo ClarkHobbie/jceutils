f462bf8baa08bfb7405ad0d062ac5c8b6d293ce4
clark <clark.hobbie@gmail.com>
2025-07-05 13:59:06 -0600

    added a test for HexConverter

---
+++ src/test/java/com/ltsllc/jceutils/HexConverterTest.java
@@ -0,0 +1,32 @@
+package com.ltsllc.jceutils;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class HexConverterTest {
+
+    @Test
+    void toHexString() {
+    }
+
+    @Test
+    void testToHexString() {
+    }
+
+    @Test
+    void testToHexString1() {
+    }
+
+    @Test
+    void testToHexString2() {
+    }
+
+    @Test
+    void toByteArray() {
+    }
+
+    @Test
+    void toByte() {
+    }
+}
